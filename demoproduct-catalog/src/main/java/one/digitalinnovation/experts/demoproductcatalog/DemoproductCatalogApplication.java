35105741c85c0b50b86260edf102d8b73493f4df
Samuel Barbosa Almeida <barbosasamuel24@gmail.com>
2021-08-31 21:11:45 -0300

    Creating Spring Cloud project

---
+++ demoproduct-catalog/src/main/java/one/digitalinnovation/experts/demoproductcatalog/DemoproductCatalogApplication.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.experts.demoproductcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoproductCatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoproductCatalogApplication.class, args);
+	}
+
+}
---
+++ demoproduct-catalog/src/test/java/one/digitalinnovation/experts/demoproductcatalog/DemoproductCatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package one.digitalinnovation.experts.demoproductcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoproductCatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
